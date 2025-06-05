8518593dfec6e66644277ec0e37e15fbae7d4ae9
Jeziel Almeida <jeziel.almeida16@gmail.com>
2025-06-04 23:16:28 -0300

    Implementar a validação do email e mensagens de erros desse input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Login', () => {
+
+  test('Dado email, caso seja apagado, mostrar mensagem de erro email obrigatório', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "qualquerValor");
+    await userEvent.clear(email);
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).not.toBeNull();
+  })
+
+  test('Dado email, caso exista algum valor, esconder mensagem de erro email obrigatório', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "qualquerValor");
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
+  })
+
+  test('Dado email, quando o campo estiver inalterado, esconder mensagem de erro email obrigatório', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
+  })
+
+  test('Dado email, quando inválido, mostrar mensagem de erro email inválido', async () => {
+    render(<App />)
+
+    const email = screen.getByTestId('email');
+  
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId('email-invalid');
+    expect(requiredError).not.toBeNull();
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const [form, setForm] = useState({
+    email: {
+      value: "",
+      hasChanged: false
+    }
+  })
+
+  const isEmailValid = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
   return (
     <main className='centralize'>
       <form>
-        <input type="email" name="" id="" placeholder='E-mail' />
+        
+        <input 
+          type="email" 
+          placeholder='E-mail' 
+          data-testid='email'
+          value={form.email.value}
+          onChange={event => setForm({...form, email: {
+            hasChanged: true, 
+            value: event.target.value
+          }})}
+        />
+        { 
+          !form.email.value 
+          && form.email.hasChanged 
+          && <div className='mensagem-erro' data-testid="email-required">E-mail é obrigatório</div> 
+        }
+        
+        { 
+          !isEmailValid(form.email.value)
+          && form.email.hasChanged
+          && <div className='mensagem-erro' data-testid="email-invalid">E-mail inválido</div> 
+        }
+        
+        
         <input type="password" name="" id="" placeholder='Senha' />
 
         <button type="button" className='clear'>Recuperar senha</button>
